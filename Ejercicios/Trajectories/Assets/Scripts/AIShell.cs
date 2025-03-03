bb3895927fe7f71ef04dad8ab5a5e55ecccf837b
Oscar Devs <ohernandezba@gmail.com>
2025-03-03 23:47:21 +0100

    Trajectories

---
+++ Ejercicios/Trajectories/Assets/Scripts/AIShell.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIShell : MonoBehaviour
+{
+    public GameObject explosion;
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "tank")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Ejercicios/Trajectories/Assets/Scripts/FireShell.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireShell : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject turret;
+    public GameObject enemy;
+
+    float rotSpeed = 2f;
+
+    void CreateBullet()
+    {
+
+        Instantiate(bullet, turret.transform.position, turret.transform.rotation);
+    }
+
+    void Update()
+    {
+        Vector3 direction = (enemy.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateBullet();
+        }
+    }
+}
