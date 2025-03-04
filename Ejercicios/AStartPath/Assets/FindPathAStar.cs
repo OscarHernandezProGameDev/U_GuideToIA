bbfe79cb00c4cdc94c6b5093a771b98ea6fbaf0d
Oscar Devs <ohernandezba@gmail.com>
2025-03-04 23:32:28 +0100

    The A* Pathfinding Algorithm

--- Ejercicios/AStartPath/Assets/FindPathAStar.cs
+++ Ejercicios/AStartPath/Assets/FindPathAStar.cs
@@ -38,5 +38,59 @@ public class PathMaker
 
 public class FindPathAStar : MonoBehaviour
 {
+    public Maze maze;
+    public Material closedMaterial;
+    public Material openMaterial;
+    public GameObject start;
+    public GameObject end;
+    public GameObject pathP;
 
+    List<PathMaker> open = new List<PathMaker>();
+    List<PathMaker> closed = new List<PathMaker>();
+
+    PathMaker goalNode;
+    PathMaker startNode;
+    PathMaker lastPos;
+    bool done = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            BeginSearch();
+        }
+    }
+    void RemoveAllMarkers()
+    {
+        GameObject[] markers = GameObject.FindGameObjectsWithTag("marker");
+
+        foreach (GameObject marker in markers)
+        {
+            Destroy(marker);
+        }
+    }
+
+    void BeginSearch()
+    {
+        done = false;
+        RemoveAllMarkers();
+
+        List<MapLocation> locations = new List<MapLocation>();
+
+        for (int z = 1; z < maze.depth - 1; z++)
+            for (int x = 1; x < maze.width - 1; x++)
+            {
+                if (maze.map[x, z] != 1)
+                    locations.Add(new MapLocation(x, z));
+            }
+
+        locations.Shuffle();
+
+        Vector3 startLocation = new Vector3(locations[0].x * maze.scale, 0, locations[0].z * maze.scale);
+
+        startNode = new PathMaker(new MapLocation(locations[0].x, locations[0].z), 0, 0, 0, Instantiate(start, startLocation, Quaternion.identity), null);
+
+        Vector3 goalLocation = new Vector3(locations[1].x * maze.scale, 0, locations[1].z * maze.scale);
+
+        goalNode = new PathMaker(new MapLocation(locations[1].x, locations[1].z), 0, 0, 0, Instantiate(end, goalLocation, Quaternion.identity), null);
+    }
 }
