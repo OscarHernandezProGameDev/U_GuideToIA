8a71d961dff97c139f1d130c3e6a956e49d3cea9
Oscar Devs <ohernandezba@gmail.com>
2025-03-07 17:38:37 +0100

    A Simple Graph API

--- Ejercicios/WPAStart/Assets/Graphs/Graph.cs
+++ Ejercicios/WPAStart/Assets/Graphs/Graph.cs
@@ -37,4 +37,96 @@ public class Graph
 
         return null;
     }
+
+    public bool AStar(GameObject startId, GameObject endId)
+    {
+        Node start = FindNode(startId);
+        Node end = FindNode(endId);
+
+        if (start == null || end == null)
+            return false;
+
+        List<Node> open = new List<Node>();
+        List<Node> closed = new List<Node>();
+        float tentative_g_score = 0;
+        bool tentative_is_better = false;
+
+        start.g = 0;
+        start.h = Distance(start, end);
+        start.f = start.h;
+
+        open.Add(start);
+        while (open.Count > 0)
+        {
+            int i = lowestF(open);
+            Node thisNode = open[i];
+
+            if (thisNode.GetId() == endId)
+            {
+                // ReconstructPath(start,end);
+
+                return true;
+            }
+
+            open.RemoveAt(i);
+            closed.Add(thisNode);
+
+            Node neightbour;
+
+            foreach (Edge e in thisNode.edgeList)
+            {
+                neightbour = e.endNode;
+
+                if (closed.IndexOf(neightbour) > -1)
+                    continue;
+
+                tentative_g_score = thisNode.g + Distance(thisNode, neightbour);
+                if (open.IndexOf(neightbour) == -1)
+                {
+                    open.Add(neightbour);
+                    tentative_is_better = true;
+                }
+                else if (tentative_g_score < neightbour.g)
+                    tentative_is_better = true;
+                else
+                    tentative_is_better = false;
+
+                if (tentative_is_better)
+                {
+                    neightbour.cameFrom = thisNode;
+                    neightbour.g = tentative_g_score;
+                    neightbour.h = Distance(neightbour, end);
+                    neightbour.f = neightbour.g + neightbour.h;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    float Distance(Node a, Node b)
+    {
+        return Vector3.SqrMagnitude(a.GetId().transform.position - b.GetId().transform.position);
+    }
+
+    int lowestF(List<Node> l)
+    {
+        float lowestf = 0;
+        int count = 0;
+        int iteratorCount = 0;
+
+        lowestf = l[0].f;
+
+        for (int i = 1; i < l.Count; i++)
+        {
+            if (l[i].f < lowestf)
+            {
+                lowestf = l[i].f;
+                iteratorCount = i;
+            }
+            count++;
+        }
+
+        return iteratorCount;
+    }
 }
