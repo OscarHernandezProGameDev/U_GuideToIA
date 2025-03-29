75dc1b74a6e85b31bcf70e936a046c68b21a7603
Oscar Devs <ohernandezba@gmail.com>
2025-03-29 09:45:48 +0100

    Adding a Progress Tracker

--- Ejercicios/Car/Assets/AIController.cs
+++ Ejercicios/Car/Assets/AIController.cs
@@ -7,11 +7,14 @@ public class AIController : MonoBehaviour
     public Circuit circuit;
     public float steeringSensitivity = 0.01f;
     public GameObject brakeLight;
+    public GameObject tracker;
 
     Drive[] ds;
     Vector3 target;
     Rigidbody rb;
     int currentWP = 0;
+    int currentTracker = 0;
+    float lookAhead = 40;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +22,21 @@ public class AIController : MonoBehaviour
         ds = GetComponentsInChildren<Drive>();
         target = circuit.waypoints[currentWP].transform.position;
         rb = GetComponent<Rigidbody>();
+
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        //tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = transform.position;
+        tracker.transform.rotation = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ProgressTracer();
+
+        target = tracker.transform.position;
+
         Vector3 localTarget = transform.InverseTransformPoint(target);
         float distanceToTarget = Vector3.Distance(target, transform.position);
         float targetAngle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
@@ -58,12 +71,30 @@ public class AIController : MonoBehaviour
             brakeLight.SetActive(false);
         }
 
+        /*
         if (distanceToTarget < 4)
         {
             currentWP++;
             if (currentWP >= circuit.waypoints.Length)
                 currentWP = 0;
             target = circuit.waypoints[currentWP].transform.position;
+        }
+        */
+    }
+
+    void ProgressTracer()
+    {
+        Debug.DrawLine(transform.position, tracker.transform.position);
+        if (Vector3.Distance(transform.position, tracker.transform.position) > lookAhead)
+            return;
+
+        tracker.transform.LookAt(circuit.waypoints[currentWP].transform.position);
+        tracker.transform.Translate(0, 0, 1.0f);
+        if (Vector3.Distance(tracker.transform.position, circuit.waypoints[currentWP].transform.position) < 1)
+        {
+            currentWP++;
+            if (currentWP >= circuit.waypoints.Length)
+                currentWP = 0;
         }
     }
 }
