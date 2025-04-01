3aff1c3ad04bb23b792a3d61efc5d9a97e805d43
Oscar Devs <ohernandezba@gmail.com>
2025-04-01 08:46:18 +0200

    Patrolling

--- Ejercicios/FiniteStateMachines/Assets/State.cs
+++ Ejercicios/FiniteStateMachines/Assets/State.cs
@@ -23,7 +23,7 @@ public class State
     protected EVENT stage;
     protected GameObject npc;
     protected NavMeshAgent agent;
-    protected Animation anim;
+    protected Animator anim;
     protected Transform player;
     protected State nextState;
 
@@ -31,7 +31,7 @@ public class State
     float visAngle = 30f;
     float shootDist = 7f;
 
-    public State(GameObject _npc, NavMeshAgent _agent, Animation _anim, Transform _player)
+    public State(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
     {
         npc = _npc;
         agent = _agent;
@@ -56,5 +56,73 @@ public class State
         }
 
         return this;
+    }
+}
+
+public class Idle : State
+{
+    public Idle(GameObject _npc, NavMeshAgent _agent, Animation _anim, Transform _player) : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.IDLE;
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isIdle");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+        {
+            nextState = new Patrol(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+        base.Update();
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isIdle");
+        base.Exit();
+    }
+}
+
+public class Patrol : State
+{
+    int currentIndex;
+
+    public Patrol(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player) : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.PATROL;
+        agent.speed = 2;
+        agent.isStopped = false;
+    }
+
+    public override void Enter()
+    {
+        currentIndex = 0;
+        anim.SetTrigger("isWalking");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        if (agent.remainingDistance < 1)
+        {
+            if (currentIndex >= GameEnvironment.Instance.Checkpoints.Count - 1)
+                currentIndex = 0;
+            else
+                currentIndex++;
+            agent.SetDestination(GameEnvironment.Instance.Checkpoints[currentIndex].transform.position);
+        }
+        base.Update();
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isWalking");
+        base.Exit();
     }
 }
