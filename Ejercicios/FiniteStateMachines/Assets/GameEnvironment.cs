39c56805c6289353b16b623f3bc43aaa7bc72d66
Oscar Devs <ohernandezba@gmail.com>
2025-04-01 08:24:48 +0200

    Creating a State Class

---
+++ Ejercicios/FiniteStateMachines/Assets/GameEnvironment.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment
+{
+    private static GameEnvironment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get => checkpoints; }
+    public static GameEnvironment Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+
+            return instance;
+        }
+    }
+}
