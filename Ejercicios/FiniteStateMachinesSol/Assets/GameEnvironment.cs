87c4eec1a06806d78aec37d713367089b2698eb0
bugbit <ohernandezba@gmail.com>
2025-04-01 23:03:35 +0200

    FSM Challenge

---
+++ Ejercicios/FiniteStateMachinesSol/Assets/GameEnvironment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+public sealed class GameEnvironment
+{
+    private static GameEnvironment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get => checkpoints; }
+    public static GameEnvironment Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+                instance.checkpoints = instance.checkpoints.OrderBy(waypoint => waypoint.name).ToList();
+            }
+
+            return instance;
+        }
+    }
+}
