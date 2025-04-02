3c5e48197c7172eaf13a591f3c6cc5be69b2bdbe
bugbit <ohernandezba@gmail.com>
2025-04-02 22:46:01 +0200

    Evade

--- Ejercicios/SteeringBehaviours/Assets/Bot.cs
+++ Ejercicios/SteeringBehaviours/Assets/Bot.cs
@@ -8,11 +8,13 @@ public class Bot : MonoBehaviour
     public GameObject target;
 
     NavMeshAgent agent;
+    Drive ds;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        ds = target.GetComponent<Drive>();
     }
 
     // Update is called once per frame
@@ -20,7 +22,8 @@ public class Bot : MonoBehaviour
     {
         //Seek(target.transform.position);
         //Flee(target.transform.position);
-        Pursue();
+        //Pursue();
+        Evade();
     }
 
     void Seek(Vector3 location)
@@ -40,15 +43,23 @@ public class Bot : MonoBehaviour
         float relativeHeading = Vector3.Angle(transform.forward, transform.TransformVector(target.transform.forward));
         float toTarget = Vector3.Angle(transform.forward, transform.TransformVector(targetDir));
 
-        if ((relativeHeading > 90 && toTarget < 20) || target.GetComponent<Drive>().currentSpeed < 0.01f)
+        if ((relativeHeading > 90 && toTarget < 20) || ds.currentSpeed < 0.01f)
         {
             Seek(target.transform.position);
 
             return;
         }
 
-        float lookAHead = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        float lookAHead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
 
         Seek(target.transform.position + target.transform.forward * lookAHead);
+    }
+
+    void Evade()
+    {
+        Vector3 targetDir = target.transform.position - transform.position;
+        float lookAHead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
+
+        Flee(target.transform.position + target.transform.forward * lookAHead);
     }
 }
