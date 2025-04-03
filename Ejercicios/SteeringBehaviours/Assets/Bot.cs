f4e7f43fbfad61f4b208a9c14609c537adf245cd
Oscar Devs <ohernandezba@gmail.com>
2025-04-03 08:34:53 +0200

    Hide

--- Ejercicios/SteeringBehaviours/Assets/Bot.cs
+++ Ejercicios/SteeringBehaviours/Assets/Bot.cs
@@ -25,7 +25,7 @@ public class Bot : MonoBehaviour
         //Pursue();
         //Evade();
         //Wander();
-        Hide();
+        CleverHide();
     }
 
     void Seek(Vector3 location)
@@ -101,5 +101,37 @@ public class Bot : MonoBehaviour
         }
 
         Seek(choseSpot);
+    }
+
+    void CleverHide()
+    {
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+        Vector3 chosenDir = Vector3.zero;
+        GameObject chosenGO = World.Instance.GetHidingSpots()[0];
+
+        for (int i = 0; i < World.Instance.GetHidingSpots().Length; i++)
+        {
+            Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
+            // 10 depende del ancho del objeto, si es muy grande puede pasar que el punto este dentro del objeto
+            Vector3 hidePos = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 10;
+
+            if (Vector3.Distance(transform.position, hidePos) < dist)
+            {
+                chosenSpot = hidePos;
+                dist = Vector3.Distance(transform.position, hidePos);
+                chosenDir = hideDir;
+                chosenGO = World.Instance.GetHidingSpots()[i];
+            }
+        }
+
+        Collider hideCol = chosenGO.GetComponent<Collider>();
+        Ray backRay = new Ray(chosenSpot, -chosenDir.normalized);
+        RaycastHit info;
+        float distance = 100.0f;
+
+        hideCol.Raycast(backRay, out info, distance);
+
+        Seek(info.point + chosenDir.normalized * 2);
     }
 }
