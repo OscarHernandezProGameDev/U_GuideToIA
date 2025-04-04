efbc91138b1dee76830149cd79716dcefa049936
Oscar Devs <ohernandezba@gmail.com>
2025-04-04 18:26:22 +0200

    Flock Challenge Project

---
+++ Ejercicios/FlockingChallenge/Assets/FlockManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public static FlockManager FM;
+    public GameObject fishPrefab;
+    public int numFish = 20;
+    public GameObject[] allFish;
+    public Vector3 swimLimits = new Vector3(5, 5, 5);
+    public Vector3 goalPos = Vector3.zero;
+
+    [Header("Fish Settings")]
+    [Range(0f, 5f)]
+    public float minSpeed;
+    [Range(0f, 5f)]
+    public float maxSpeed;
+    [Range(1f, 10f)]
+    public float neighbourDistance;
+    [Range(1f, 5f)]
+    public float rotationSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allFish = new GameObject[numFish];
+
+        for (int i = 0; i < allFish.Length; i++)
+        {
+            Vector3 pos = transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x), Random.Range(-swimLimits.y, swimLimits.y), Random.Range(-swimLimits.z, swimLimits.z));
+
+            allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+        }
+        FM = this;
+        goalPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+        {
+            goalPos = transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x), Random.Range(-swimLimits.y, swimLimits.y), Random.Range(-swimLimits.z, swimLimits.z));
+        }
+    }
+}
