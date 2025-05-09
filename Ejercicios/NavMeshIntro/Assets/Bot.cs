2f57ce977cc853fe5f948c8042a59aef6683016f
Oscar Devs <ohernandezba@gmail.com>
2025-05-09 18:58:22 +0200

    NavMesh

---
+++ Ejercicios/NavMeshIntro/Assets/Bot.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bot : MonoBehaviour
+{
+    RaycastHit raycashit = new RaycastHit();
+    NavMeshAgent agent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycashit))
+            {
+                agent.SetDestination(raycashit.point);
+            }
+        }
+    }
+}
