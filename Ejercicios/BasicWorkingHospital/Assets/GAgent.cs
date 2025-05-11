cfe9681ff30d564d3fec53cdbc30964c59cf0399
Oscar Devs <ohernandezba@gmail.com>
2025-05-11 11:12:14 +0200

    Creating a Multistep Plan

--- Ejercicios/BasicWorkingHospital/Assets/GAgent.cs
+++ Ejercicios/BasicWorkingHospital/Assets/GAgent.cs
@@ -1,20 +1,17 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
 public class SubGoal
 {
-
-    public Dictionary<string, int> sGoals;
+    public Dictionary<string, int> sgoals;
     public bool remove;
-
 
     public SubGoal(string s, int i, bool r)
     {
-
-        sGoals = new Dictionary<string, int>();
-        sGoals.Add(s, i);
+        sgoals = new Dictionary<string, int>();
+        sgoals.Add(s, i);
         remove = r;
     }
 }
@@ -23,29 +20,25 @@ public class GAgent : MonoBehaviour
 {
     public List<GAction> actions = new List<GAction>();
     public Dictionary<SubGoal, int> goals = new Dictionary<SubGoal, int>();
-    //public GInventory inventory = new GInventory();
     public WorldStates beliefs = new WorldStates();
-    bool invoked = false;
 
     GPlanner planner;
     Queue<GAction> actionQueue;
     public GAction currentAction;
     SubGoal currentGoal;
 
-    protected virtual void Start()
+    // Start is called before the first frame update
+    public void Start()
     {
-
         GAction[] acts = this.GetComponents<GAction>();
         foreach (GAction a in acts)
-        {
-
             actions.Add(a);
-        }
     }
 
+
+    bool invoked = false;
     void CompleteAction()
     {
-
         currentAction.running = false;
         currentAction.PostPerform();
         invoked = false;
@@ -53,17 +46,12 @@ public class GAgent : MonoBehaviour
 
     void LateUpdate()
     {
-
         if (currentAction != null && currentAction.running)
         {
-
-            float distanceToTarget = Vector3.Distance(currentAction.target.transform.position, this.transform.position);
-            if (currentAction.agent.hasPath && distanceToTarget < 2.0f)//currentAction.agent.remainingDistance < 0.5f)
+            if (currentAction.agent.hasPath && currentAction.agent.remainingDistance < 1f)
             {
-
                 if (!invoked)
                 {
-
                     Invoke("CompleteAction", currentAction.duration);
                     invoked = true;
                 }
@@ -73,18 +61,15 @@ public class GAgent : MonoBehaviour
 
         if (planner == null || actionQueue == null)
         {
-
             planner = new GPlanner();
+
             var sortedGoals = from entry in goals orderby entry.Value descending select entry;
 
             foreach (KeyValuePair<SubGoal, int> sg in sortedGoals)
             {
-
-                actionQueue = planner.plan(actions, sg.Key.sGoals, beliefs);
-
+                actionQueue = planner.plan(actions, sg.Key.sgoals, null);
                 if (actionQueue != null)
                 {
-
                     currentGoal = sg.Key;
                     break;
                 }
@@ -93,10 +78,8 @@ public class GAgent : MonoBehaviour
 
         if (actionQueue != null && actionQueue.Count == 0)
         {
-
             if (currentGoal.remove)
             {
-
                 goals.Remove(currentGoal);
             }
             planner = null;
@@ -104,30 +87,24 @@ public class GAgent : MonoBehaviour
 
         if (actionQueue != null && actionQueue.Count > 0)
         {
-
             currentAction = actionQueue.Dequeue();
-
             if (currentAction.PrePerform())
             {
-
                 if (currentAction.target == null && currentAction.targetTag != "")
-                {
-
                     currentAction.target = GameObject.FindWithTag(currentAction.targetTag);
-                }
 
                 if (currentAction.target != null)
                 {
-
                     currentAction.running = true;
                     currentAction.agent.SetDestination(currentAction.target.transform.position);
                 }
             }
             else
             {
-
                 actionQueue = null;
             }
+
         }
+
     }
 }
