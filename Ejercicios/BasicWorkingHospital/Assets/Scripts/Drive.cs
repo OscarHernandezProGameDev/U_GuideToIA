4dbe6b2c43d2920998cb25baaf587912d8dd3c21
Oscar Devs <ohernandezba@gmail.com>
2025-05-16 15:20:47 +0200

    Moving the Camera

--- Ejercicios/BasicWorkingHospital/Assets/Scripts/Drive.cs
+++ Ejercicios/BasicWorkingHospital/Assets/Scripts/Drive.cs
@@ -10,7 +10,8 @@ public class Drive : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        cam = GetComponentInChildren<Camera>();
+        cam.gameObject.transform.LookAt(transform.position);
     }
 
     // Update is called once per frame
@@ -21,5 +22,32 @@ public class Drive : MonoBehaviour
 
         this.transform.Translate(0, 0, translation);
         this.transform.Translate(translation2, 0, 0);
+
+        if (Input.GetKey(KeyCode.Z))
+            this.transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+
+        if (Input.GetKey(KeyCode.C))
+            this.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+
+        if (Input.GetKey(KeyCode.R) && cam.gameObject.transform.position.y > 5)
+            cam.gameObject.transform.Translate(0, 0, speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.F) && cam.gameObject.transform.position.y < 45)
+            cam.gameObject.transform.Translate(0, 0, -speed * Time.deltaTime);
+
+        float angle = Vector3.Angle(cam.gameObject.transform.forward, Vector3.up);
+        //Debug.Log(angle);
+
+        if (Input.GetKey(KeyCode.T) && angle < 175)
+        {
+            cam.gameObject.transform.Translate(Vector3.up);
+            cam.gameObject.transform.LookAt(transform.position);
+        }
+
+        if (Input.GetKey(KeyCode.G) && angle > 95)
+        {
+            cam.gameObject.transform.Translate(Vector3.down);
+            cam.gameObject.transform.LookAt(transform.position);
+        }
     }
 }
