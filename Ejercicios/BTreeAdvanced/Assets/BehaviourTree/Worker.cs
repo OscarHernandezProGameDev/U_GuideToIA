00a8b27f5779f0e9d312019500537c92c576f417
OscarDevNet <ohernandezba@gmail.com>
2026-07-04 12:22:29 +0200

    Thinking like a Behaviour Tree

--- Ejercicios/BTreeAdvanced/Assets/BehaviourTree/Worker.cs
+++ Ejercicios/BTreeAdvanced/Assets/BehaviourTree/Worker.cs
@@ -10,21 +10,42 @@ public class Worker : BTAgent
     public override void Start()
     {
         base.Start();
+        Leaf allocatePatron = new Leaf("Allocate Patron", AllocatePatron);
         Leaf goToPatron = new Leaf("Go To Patron", GoToPatron);
         Leaf goToOffice = new Leaf("Go To Office", GoToOffice);
 
+        Sequence getPatron = new Sequence("Find a patron");
+        getPatron.AddChild(allocatePatron);
+        getPatron.AddChild(goToPatron);
+
         Selector beWorker = new Selector("Be a Worker");
-        beWorker.AddChild(goToPatron);
+        beWorker.AddChild(getPatron);
         beWorker.AddChild(goToOffice);
 
         tree.AddChild(beWorker);
     }
 
-    public Node.Status GoToPatron()
+    // public Node.Status PatronWaiting()
+    // {
+    //     
+    // }
+
+    public Node.Status AllocatePatron()
     {
         if (Blackboard.Instance.patrons.Count == 0)
             return Node.Status.FAILURE;
+
         patron = Blackboard.Instance.patrons.Pop();
+        if (patron == null)
+            return Node.Status.FAILURE;
+
+        return Node.Status.SUCCESS;
+    }
+
+    public Node.Status GoToPatron()
+    {
+        if (patron == null)
+            return Node.Status.FAILURE;
         Node.Status s = GoToLocation(patron.transform.position);
         if (s == Node.Status.SUCCESS)
         {
@@ -38,7 +59,7 @@ public class Worker : BTAgent
     public Node.Status GoToOffice()
     {
         Node.Status s = GoToLocation(office.transform.position);
-
+        patron = null;
         return s;
     }
 }
